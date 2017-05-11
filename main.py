68b61fe82c762f44e3d5a953a5b7c674fabdd577
Giovanni Carvelli <giovannircarvelli@gmail.com>
2017-05-10 23:01:52 -0400

    Move clientid to json file

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ import os
 import os.path
 import sys
 import urllib
+import json
 
-keyfile = open('clientid.txt')
+
 global clientid
-clientid = keyfile.read().strip()
-keyfile.close()
 
 def main():
     os.mkdir('images')
+    keyfile = open('config.json')
+    data = json.loads(keyfile.read())
+    if "clientid" not in data:
+        print("warning: clientid field isn't in json")
+    else:
+        global clientid
+        clientid = data["clientid"]
+        print("client id: %s" % clientid)
+
     with open('subreddits.txt') as f:
         
         for sub in [line.strip() for line in f if not line.isspace()]:
